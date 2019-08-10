c665c828698c14c4f0f7a9864d5d03da2aa3c051
iwansafr <iwansafr@gmail.com>
2019-08-10 19:18:22 +0700

    title

--- application/modules/home/views/desa/tanpa_perangkat.php
+++ application/modules/home/views/desa/tanpa_perangkat.php
@@ -3,13 +3,22 @@
 if(!empty($desa_tanpa_perangkat))
 {
 	$kelompok_id = ['1'=>'perangkat desa', '2'=>'bpd','3'=>'lpmp','4'=>'pkk','5'=>'karang taruna','6'=>'rt','7'=>'rw','8'=>'kpmd','9'=>'linmas'];
+	$kelompok_min = ['1'=>5, '2'=>5,'3'=>5,'4'=>5,'5'=>5,'6'=>3,'7'=>2,'8'=>1,'9'=>5];
 	$title = ['uncomplete'=>'Data Kosong','kurang'=>'Data kurang','complete'=>'Data Cukup'];
 	foreach ($desa_tanpa_perangkat as $key => $value) 
 	{
-		echo '<h1>'.$title[$key].'</h1>';
+		$title_status = '';
+		// echo '<h1>'.$title[$key].' '.$title_status.'</h1>';
 		foreach($value AS $vkey => $vvalue)
 		{
-			echo '<h2>'.$kelompok_id[$vkey].' ('.$vvalue['total'].' desa)</h2>';
+			if($key == 'kurang'){
+				$title_status = 'data kurang dari '.$kelompok_min[$vkey];
+			}else if($key == 'complete'){
+				$title_status = 'data lebih dari/pas '.@intval($kelompok_min[$vkey]);
+			}else{
+				$title_status = 'data kosong';
+			}
+			echo '<h2>'.$kelompok_id[$vkey].' ('.$vvalue['total'].' desa) '.$title_status.'</h2>';
 			switch ($key) {
 				case 'uncomplete':
 					foreach ($vvalue['data'] as $dkey => $dvalue) 
